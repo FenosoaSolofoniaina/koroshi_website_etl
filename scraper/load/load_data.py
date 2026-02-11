bf36e59f9271d5db6fb6ca108ddfab0b6e1020c6
Fenosoa <solofoniainafenosoa020@gmail.com>
2026-02-11 18:46:38 +0300

    Update files : fix bug on 'main.py'

--- scraper/load/load_data.py
+++ scraper/load/load_data.py
@@ -1,15 +1,19 @@
 import logging
-from typing import Any, Union
+from typing import Union
 import polars as pl
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+
 
 
 # ==================================================================================================================================================================== #
-# ======================================================================= KoroshiDataLoader ======================================================================= #
+# ======================================================================= KoroshiDataBaseLoader ======================================================================= #
 # ==================================================================================================================================================================== #
-class KoroshiDataLoader() :
+class KoroshiDataBaseLoader() :
 
 
     def __init__(self,
@@ -79,5 +83,76 @@ class KoroshiDataLoader() :
                 
             self.logger.info(f"Data injected into the table '{self.schema}.{self.table}'")
 
+
+
+
+# ==================================================================================================================================================================== #
+# ======================================================================= KoroshiDataCloudLoader ======================================================================= #
+# ==================================================================================================================================================================== #
+class KoroshiDataCloudLoader() :
+
+
+    def __init__(self,
+                 service_account_fp: str,
+                 project: str,
+                 dataset: str,
+                 table: str,
+                 file_log: str) -> None :
+        """
+        Provide some feature to connect and insert data into a database (in this case, a PostgreSQL)
+            
+            Args
+                connection_url : [string] : the url that provides information about how to connect to the database
+                table : [string] : the table name where insert data into the database
+                file_log : [string] : file path where write the log
+                schema : [string or None] : the schema which contains the table
+                        if None, that means the schema is public or default schema
+                        :default:None
+
+            Raises
+                [SQLAlchemyError] : when an error occurred during connecting to the database
+        """
+
+        # Set the log management
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename=file_log, mode='w')
+        handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s()] %(message)s'))
+        self.logger.addHandler(handler)
+
+        #print(type(service_account_fp))
+        credentials = service_account.Credentials.from_service_account_file(filename=service_account_fp)
+        self.client = bigquery.Client(project=project, credentials=credentials)
+        self.bg_table_id = f"{project}.{dataset}.{table}"
+
+        self.logger.info("BigQuery client initialized")
+
+    
+    def load_data(self,
+                    dataframe: pl.DataFrame) -> None :
+        """
+        Insert data into the database
+            
+            Args
+                dataframe : [pl.DataFrame] : the dataframe that contains data
+        """
+
+        job_config = bigquery.LoadJobConfig(
+            source_format=bigquery.SourceFormat.PARQUET,
+            autodetect=True,
+            write_disposition="WRITE_APPEND"  # ou WRITE_TRUNCATE
+        )
+
+        temp_data_fp = 'data.parquet'
+        dataframe.write_parquet(temp_data_fp)
+        with open(temp_data_fp, "rb") as f:
+            load_job = self.client.load_table_from_file(
+                file_obj=f,
+                destination=self.bg_table_id,
+                job_config=job_config
+            )
+
+        load_job.result()
+        self.logger.info("Chargement batch réussi 🎉")
 
     
--- scraper/main.py
+++ scraper/main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 from utils.utilities import read_json, convert_json_to_dataframe
 from utils.extract import get_all_products_data, get_all_products_list
+from utils.transform import preprocessing
+from utils.load import load_data_to_db, load_data_to_bigquery
             
 
 # ================================================================================================================== #
@@ -16,6 +18,7 @@ def main() -> None :
     """
     
     logging.info("======================= PROGRAM STARTED =======================")
+    print("======================= PROGRAM STARTED =======================")
 
     current_time = datetime.now()
     
@@ -27,7 +30,8 @@ def main() -> None :
     logging.basicConfig(filename=os.path.join(BASE_DIR, 'logs/main.log'),
                         filemode='w',
                         format='[%(asctime)s] [%(levelname)s] [%(funcName)s()] %(message)s',
-                        level=logging.INFO)
+                        level=logging.INFO,
+                        force=True)
 
     # fichier de configuration nécessaire au scraping du site
     configuration_fp = os.path.join(BASE_DIR, CONFIGURATION_FP)
@@ -36,23 +40,42 @@ def main() -> None :
     if json_config is not None :
         
         # Extract and save products list
-        products_list_fp = get_all_products_list(configuration=json_config,
-                                                 log_file = os.path.join(BASE_DIR, 'logs/products_list.log')
-                                                )
+        # products_list_fp = get_all_products_list(configuration=json_config,
+        #                                          log_file = os.path.join(BASE_DIR, 'logs/products_list.log')
+        #                                         )
                                                 
         
-        # Extract and save products data
-        products_data_fp = get_all_products_data(products_list_fp=products_list_fp,
-                                                 log_file=os.path.join(BASE_DIR, 'logs/products_data.log')
-                                                )
-        
-        # Load data extracted into a PostgreSQL database
-        # load_data_to_db(data_fp=products_data_fp,
-        #                 log_file=os.path.join(BASE_DIR, 'logs/to_db.log'))
-
+        # # # Extract and save products data
+        # products_data_fp = get_all_products_data(products_list_fp=products_list_fp,
+        #                                          log_file=os.path.join(BASE_DIR, 'logs/products.log')
+        #                                         )
+        # Transform data
+        products_data_fp = "/home/fenosoa/Projects/data_engineer/koroshi/scraper/json/products_data_2026-02-11.json"
         df = convert_json_to_dataframe(fp=products_data_fp)
+        df = preprocessing(data=df,
+                           log_file=os.path.join(BASE_DIR, 'logs/data_transformations.log')
+                           )
+
+        # Load data to Big Query
+        load_data_to_db(data=df,
+                        log_file=os.path.join(BASE_DIR, 'logs/to_postgres.log')
+                        )
+        PROJECT_ID = os.getenv('BG_PROJECT_ID', '')
+        DATASET_ID = os.getenv('BQ_DATASET_ID', '')
+        TABLE_NAME = os.getenv('BQ_TABLE_NAME', '')
+        service_account_fp = "dbt-learn-bq-472715-f4-392e0df00188.json"
+
+        load_data_to_bigquery(service_account_fp=os.path.join(BASE_DIR, service_account_fp),
+                              project=PROJECT_ID,
+                              dataset=DATASET_ID,
+                              table=TABLE_NAME,
+                              data=df,
+                              log_file=os.path.join(BASE_DIR, 'logs/big_query.log')
+                              )
+
         
     logging.info("======================= PROGRAM FINISHED =======================")
+    print("======================= PROGRAM FINISHED =======================")
     time_diff = datetime.now() - current_time
     logging.info(f"Program takes {time_diff}")
     print(f"Program takes {time_diff}")
